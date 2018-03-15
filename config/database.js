53206c8e7400eb7beef628176ead628e49b899c1
Mark Briggs <briggsm@alumni.msoe.edu>
2018-03-15 17:33:59 +0300

    DB now uses EC2

--- config/database.js
+++ config/database.js
@@ -1,4 +1,7 @@
 module.exports = {
     remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
-    localUrl: 'mongodb://localhost/meanstacktutorials'
+    //remoteUrl : 'mongodb://briggsm:ballbowling@ec2-35-158-50-199.eu-central-1.compute.amazonaws.com:27017/dummyDB',
+    //localUrl: 'mongodb://localhost/meanstacktutorials'
+    localUrl: 'mongodb://briggsm2:ballbowling@ec2-35-158-50-199.eu-central-1.compute.amazonaws.com:27017/dummyDB'
+    //localUrl: 'mongodb://briggsm:ballbowling@ec2-35-158-50-199.eu-central-1.compute.amazonaws.com:27017/admin'
 };
